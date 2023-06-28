20c0a4d44094daca414f7eb8ae0e657c1e6baafd
Anapat Muangchol <anapat.mua@truedigital.com>
2023-06-28 18:10:45 +0700

    finish deploy bsc testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,27 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const provider = new HDWalletProvider({
-     privateKeys: ['{private key}'],
+const providerTestnet = new HDWalletProvider({
+     privateKeys: ['{private_key}'],
      providerOrUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/'
 });
+const providerProd = new HDWalletProvider({
+    privateKeys: ['{private_key}'],
+    providerOrUrl: 'https://bsc-dataseed.binance.org/'
+});
+
 module.exports = {
      networks: {
-          bsc: {
-               // production
-          },
-          binanceTestnet: {
-               provider: () => provider,
-               network_id: "97",
-               gas: 1000000,
-               skipDryRun: true
-          }
+            bsc: {
+                provider: () => providerTestnet,
+                network_id: "56",
+                gas: 1000000,
+                skipDryRun: true
+            },
+            binanceTestnet: {
+                provider: () => providerProd,
+                network_id: "97",
+                gas: 1000000,
+                skipDryRun: true
+            }
      },
      compilers: {
           solc: {
